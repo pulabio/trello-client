99cba111e380753f4e8ed5871b2e24152cdf5dfc
Flavio de Lima <flaviolimpio@gmail.com>
2020-04-14 00:16:09 -0300

    feature: load a Trello card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './config'
 import './App.css';
+import FetchCards from "./components/FetchCards";
 
 function App() {
   return (
     <div className="App">
+        <h1>Lista de cards</h1>
+        <FetchCards/>
     </div>
   );
 }
---
+++ src/components/FetchCards.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import config from '../config';
+
+const API_KEY = config.API.KEY;
+const API_TOKEN = config.API.TOKEN;
+const BOARD_ID = config.BOARD_ID;
+
+export default class FetchCards extends React.Component{
+  state = {
+    loading:true,
+    card: null,
+  }
+
+  async componentDidMount(){
+    const query = `cards?limit=${10}`
+    const base_url = `https://api.trello.com/1/boards/${BOARD_ID}/${query}&`;
+    const auth = `key=${API_KEY}&token=${API_TOKEN}`;
+    const url = base_url+auth;
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({cards: data, loading:false})
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.loading || !this.state.cards ? (
+          <div>loading...</div> 
+        ) : (
+         <div>
+           {this.state.cards.map(card => <div> {card.name}</div>)}
+         </div>)
+        }
+      </div>
+    );
+  }
+};
