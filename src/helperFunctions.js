dc7def0eb7d7db1fd546ea864f71671ab705fe7d
Flavio de Lima <flaviolimpio@gmail.com>
2020-04-17 10:05:37 -0300

    refactor: simpliflied the load_lists function

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,8 +1,7 @@
 import config from './config';
 
-const API_KEY = config.API.KEY;
-const API_TOKEN = config.API.TOKEN;
 const BOARD_ID = config.BOARD_ID;
+const auth = config.API;
 
 export async function fetchTrello(BOARD_ID, query, auth){
   const base_url = `https://api.trello.com/1/boards/${BOARD_ID}/${query}&`;
@@ -15,18 +14,13 @@ export async function fetchTrello(BOARD_ID, query, auth){
 
 export async function load_lists(){
   const query = "lists?"
-  const base_url = `https://api.trello.com/1/boards/${BOARD_ID}/${query}&`;
-  const auth = `key=${API_KEY}&token=${API_TOKEN}`;
-  const url = base_url+auth;
-  
-  const response = await fetch(url);
-  const lists = await response.json();
+  const lists = await fetchTrello(BOARD_ID, query, auth);
 
   const board_lists = {}
-
   lists.forEach(list => {
     board_lists[list.id] = list.name  
   });
+
   let stringfied_board_lists = JSON.stringify(board_lists);
   localStorage.setItem("board_lists", stringfied_board_lists);
 }
@@ -38,5 +32,3 @@ export function buildListDict(customFieldsDef, fieldID){
   fieldDefinitions.options.forEach(option => dict[option.id]=option.value.text);
   return dict;
 }
-
-export default load_lists;
